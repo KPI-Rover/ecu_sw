42cbce137726027d17e3f66f39f4bed31a8d9793
hasslesstech <hasslesstech@tutanota.com>
2025-08-22 14:19:00 +0300

    mock timers.c

---
+++ KPI_Rover/Buzzer/mock-suite/inc/FreeRTOS.h
@@ -0,0 +1,18 @@
+#ifndef __FREERTOS_H
+#define __FREERTOS_H
+
+
+#include <stdint.h>
+
+typedef uint32_t TickType_t;
+
+#define pdFALSE 0
+#define pdTRUE 1
+
+#define pdFAIL (pdFALSE)
+#define pdPASS (pdTRUE)
+
+#define pdMS_TO_TICKS(ms) (TickType_t) ms
+
+
+#endif /* __FREERTOS_H */
