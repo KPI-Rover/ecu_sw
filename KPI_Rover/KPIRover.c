f80be908478cb5ec8a38be3e97db818a32a51822
velichko <maksimvelichko16@gmail.com>
2025-09-23 05:32:51 +0300

    Issue #24: removed dependency on FreeRTOS in the driver and other improvements

--- KPI_Rover/KPIRover.c
+++ KPI_Rover/KPIRover.c
@@ -4,11 +4,12 @@
 
 void KPIRover_Init(void) {
 
+
 	// ADC
 	const osThreadAttr_t adcTask_attributes = {
 	      .name = "adcTask",
 	      .priority = (osPriority_t) osPriorityNormal,
-	      .stack_size = 512 * 4
+	      .stack_size = 1024 * 4
 	  };
 	(void) osThreadNew(ADC_Manager_Task, NULL, &adcTask_attributes);
 
