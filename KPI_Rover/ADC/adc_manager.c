0478341893cb7c8868a563761bcef9b750b67612
velichko <maksimvelichko16@gmail.com>
2025-10-07 14:09:26 +0300

    Issue #24: added sub states for calibration, changed DMA to polling

--- KPI_Rover/ADC/adc_manager.c
+++ KPI_Rover/ADC/adc_manager.c
@@ -2,20 +2,17 @@
 #include "ulog.h"
 #include "cmsis_os.h"
 #include "stm32f4xx_hal.h"
+#include "stdbool.h"
 
 static const adc_channel_config_t adc_config[] = {
     { .channel = 2, .phys_high = 3.3f, .calibration_required = 1 }
 };
 
-static uint32_t last_log_tick = 0;
-
 #define ADC_CONFIG_COUNT (sizeof(adc_config) / sizeof(adc_config[0]))
-#define ADC_TASK_FLAG_TIMER   (1U << 0)
 
 #define ADC_TIMER_PERIOD_MS 100
 
-static osTimerId_t adc_periodic_timer = NULL;
-static osThreadId_t adcTaskHandle = NULL;
+static osTimerId_t adc_timer_handle;
 
 static uint16_t adc_last_raw[ADC_CONFIG_COUNT];
 static float    adc_last_cal[ADC_CONFIG_COUNT];
@@ -30,11 +27,12 @@ static void adc_data_cb(uint8_t channel, uint16_t value) {
     }
 }
 
-static void adc_periodic_timer_cb(void *arg) {
-	ULOG_INFO("adc_periodic_timer_cb");
-    (void)arg;
-    osThreadFlagsSet(adcTaskHandle, ADC_TASK_FLAG_TIMER);
+static volatile bool adc_timer_flag = false;
+
+static void adc_timer_callback(void *arg) {
+    adc_timer_flag = true;
 }
+
 
 void ADC_Manager_Init(void) {
     uint8_t channels[ADC_CONFIG_COUNT];
@@ -45,59 +43,55 @@ void ADC_Manager_Init(void) {
 
     ADC_Driver_Init(channels, ADC_CONFIG_COUNT);
 
-    ADC_Driver_Start();
-
     ADC_Driver_RegisterCallback(adc_data_cb);
 
-    adcTaskHandle = osThreadGetId();
+    ADC_Driver_Start();
 
-    const osTimerAttr_t timer_attr = {
-      .name = "ADC_Periodic",
-    };
-    adc_periodic_timer = osTimerNew(adc_periodic_timer_cb, osTimerPeriodic, NULL, &timer_attr);
+    const osTimerAttr_t timer_attrs = {
+			.name = "ADC_Timer"
+	};
+
+	adc_timer_handle = osTimerNew(adc_timer_callback, osTimerPeriodic, NULL, &timer_attrs);
+	if (!adc_timer_handle) {
+		ULOG_ERROR("Failed to create ADC timer");
+		osThreadExit();
+	}
+
+	osDelay(10);
+
+	if (osTimerStart(adc_timer_handle, ADC_TIMER_PERIOD_MS) != osOK) {
+		ULOG_ERROR("Failed to start ADC timer");
+		osThreadExit();
+	}
 }
 
 
 void ADC_Manager_Task(void *argument) {
-
-	osTimerStart(adc_periodic_timer, ADC_TIMER_PERIOD_MS);
-
-	ADC_Manager_Init();
+    ADC_Manager_Init();
 
     for (;;) {
-
-    	 uint32_t flags = osThreadFlagsWait(ADC_TASK_FLAG_TIMER, osFlagsWaitAny, 10);
-
-    	if (flags & ADC_TASK_FLAG_TIMER) {
-    	    ADC_Driver_TimerTask();
-    	}
-
-    	uint32_t now = HAL_GetTick();
-		if (now - last_log_tick >= 500) {
-			last_log_tick = now;
-			for (size_t i = 0; i < ADC_CONFIG_COUNT; ++i) {
-				int cal_mv = (int)(adc_last_cal[i] * 1000.0f);
-				ULOG_INFO("ADC CH%u: filtered=%u cal=%d mV",
-				adc_config[i].channel, adc_last_raw[i], cal_mv);
-			}
+    	ADC_Driver_ReadChannels();
+    	if (adc_timer_flag) {
+			adc_timer_flag = false;
+			ADC_Driver_TimerTask();
 		}
+		osDelay(10);
 
-//      int state = DB_ReadCalibState();
-
-      switch (0) { // (state)
-//        case 1: // start 0В
-//          ADC_Driver_StartCalibrationLow(adc_config[0].channel);
-//              DB_WriteCalibState(2);
-//              break;
+//        int state = DB_ReadCalibState();
 //
-//          case 3: // start 3.3В
-//            ADC_Driver_StartCalibrationHigh(adc_config[0].channel);
-//              DB_WriteCalibState(4);
-//              break;
-
-         default:
-           break;
-      }
-      osDelay(100);
+//		switch (0) { // (state)
+//	        case 1: // start 0В
+//	            ADC_Driver_StartCalibrationLow(adc_config[0].channel);
+//		        DB_WriteCalibState(2);
+//		        break;
+//
+//		    case 3: // start 3.3В
+//		        ADC_Driver_StartCalibrationHigh(adc_config[0].channel);
+//	            DB_WriteCalibState(4);
+//		        break;
+//
+//			default:
+//			    break;
+//		}
     }
 }
