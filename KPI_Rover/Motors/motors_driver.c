43dc011f085fb22c0b2f955440d0dea8944ff994
velichko <maksimvelichko16@gmail.com>
2025-11-18 14:00:06 +0200

    Issue #17: fixed architecture

--- KPI_Rover/Motors/motors_driver.c
+++ KPI_Rover/Motors/motors_driver.c
@@ -1,123 +1,42 @@
 #include "motors_driver.h"
 #include "ulog.h"
 
-void DriverMotors_Init(Motor_HandleTypeDef* motor){
+void DriverMotor_Init(drvMotor_t* motor){
 	HAL_TIM_PWM_Start(motor->htim_pwm, motor->pwm_channel);
 	__HAL_TIM_MOE_ENABLE(motor->htim_pwm);
-	DriverMotors_Stop(motor);
+
+	DriverMotor_setPwm(motor, 0);
+	DriverMotor_setDirection(motor, true);
+	motor->enabled = false;
+
+	ULOG_INFO("drvMotor_init: PWM on ch=%lu", motor->pwm_channel);
 }
 
-void DriverMotors_Forward(Motor_HandleTypeDef* motor){
-	HAL_GPIO_WritePin(motor->IN1_port, motor->IN1_pin, GPIO_PIN_SET);
-	HAL_GPIO_WritePin(motor->IN2_port, motor->IN2_pin, GPIO_PIN_RESET);
+void DriverMotor_Enable(drvMotor_t* motor){
+	motor->enabled = true;
+	ULOG_INFO("Motor enabled (ch=%lu)", motor->pwm_channel);
 }
 
-void DriverMotors_Backward(Motor_HandleTypeDef* motor){
-	HAL_GPIO_WritePin(motor->IN1_port, motor->IN1_pin, GPIO_PIN_RESET);
-	HAL_GPIO_WritePin(motor->IN2_port, motor->IN2_pin, GPIO_PIN_SET);
+void DriverMotor_Disable(drvMotor_t* motor){
+	DriverMotor_setPwm(motor, 0);
+    motor->enabled = false;
+    ULOG_INFO("Motor disabled (ch=%lu)", motor->pwm_channel);
 }
 
-void DriverMotors_Stop(Motor_HandleTypeDef* motor){
-	HAL_GPIO_WritePin(motor->IN1_port, motor->IN1_pin, GPIO_PIN_RESET);
-	HAL_GPIO_WritePin(motor->IN2_port, motor->IN2_pin, GPIO_PIN_RESET);
+void DriverMotor_setDirection(drvMotor_t* motor, bool forward){
+    HAL_GPIO_WritePin(motor->IN1_port, motor->IN1_pin, forward ? GPIO_PIN_SET : GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(motor->IN2_port, motor->IN2_pin, forward ? GPIO_PIN_RESET : GPIO_PIN_SET);
+
+    ULOG_DEBUG("Motor dir=%s", forward ? "FWD" : "BWD");
 }
 
-void DriverMotors_SetSpeed(Motor_HandleTypeDef* motor, uint8_t speed_percent) {
-    if (speed_percent > 100) speed_percent = 100;
-    uint32_t period = __HAL_TIM_GET_AUTORELOAD(motor->htim_pwm);
-    uint32_t compare = (uint32_t)(( (uint64_t)(period + 1) * speed_percent ) / 100);
-    __HAL_TIM_SET_COMPARE(motor->htim_pwm, motor->pwm_channel, compare);
-    ULOG_INFO("SetSpeed %u%% -> compare=%lu period=%lu", speed_percent, compare, period);
+void DriverMotor_setPwm(drvMotor_t* motor, uint16_t pwm)
+{
+    if (!motor->enabled)
+        return;
+
+    __HAL_TIM_SET_COMPARE(motor->htim_pwm, motor->pwm_channel, pwm);
+    //ULOG_DEBUG("Motor PWM set %u (ch=%lu)", pwm, motor->pwm_channel);
 }
 
-void DriverMotors_TimerTask(Motor_HandleTypeDef* motor) {
-    uint32_t now = HAL_GetTick();
-    uint32_t elapsed = now - motor->state_entry_time_ms;
-
-    switch (motor->state) {
-
-        case MOTOR_STATE_STOP:
-
-            break;
-
-        case MOTOR_STATE_FORWARD:
-            switch (motor->substate) {
-                case MOTOR_SUBSTATE_ACCEL:
-                    if (motor->current_speed < motor->target_speed) {
-                        motor->current_speed += 15;
-                        DriverMotors_SetSpeed(motor, motor->current_speed);
-                    } else {
-                        motor->substate = MOTOR_SUBSTATE_CONSTANT;
-                    }
-                    break;
-
-                case MOTOR_SUBSTATE_CONSTANT:
-
-                    break;
-
-                case MOTOR_SUBSTATE_DECEL:
-                    if (motor->current_speed > 0) {
-                        if (motor->current_speed > 15)
-                            motor->current_speed -= 15;
-                        else
-                            motor->current_speed = 0;
-
-                        DriverMotors_SetSpeed(motor, motor->current_speed);
-                    } else {
-                        DriverMotors_SetSpeed(motor, 0);
-                        DriverMotors_Stop(motor);
-                        motor->state = MOTOR_STATE_STOP;
-                        motor->substate = MOTOR_SUBSTATE_NONE;
-                        ULOG_INFO("Motor stopped completely");
-                    }
-                    break;
-
-
-                default:
-                    break;
-            }
-            break;
-
-        case MOTOR_STATE_BACKWARD:
-        	switch (motor->substate) {
-				case MOTOR_SUBSTATE_ACCEL:
-					if (motor->current_speed < motor->target_speed) {
-						motor->current_speed += 15;
-						DriverMotors_SetSpeed(motor, motor->current_speed);
-					} else {
-						motor->substate = MOTOR_SUBSTATE_CONSTANT;
-					}
-					break;
-
-				case MOTOR_SUBSTATE_CONSTANT:
-
-					break;
-
-				case MOTOR_SUBSTATE_DECEL:
-				    if (motor->current_speed > 0) {
-				        if (motor->current_speed > 15)
-				            motor->current_speed -= 15;
-				        else
-				            motor->current_speed = 0;
-
-				        DriverMotors_SetSpeed(motor, motor->current_speed);
-				    } else {
-				        DriverMotors_SetSpeed(motor, 0);
-				        DriverMotors_Stop(motor);
-				        motor->state = MOTOR_STATE_STOP;
-				        motor->substate = MOTOR_SUBSTATE_NONE;
-				        ULOG_INFO("Motor stopped completely");
-				    }
-				    break;
-
-
-				default:
-					break;
-			}
-            break;
-
-        default:
-            break;
-    }
-}
 
--- KPI_Rover/Motors/motors_driver.h
+++ KPI_Rover/Motors/motors_driver.h
@@ -2,43 +2,25 @@
 #define MOTORS_DRIVER_H
 
 #include "stm32f4xx_hal.h"
-
-typedef enum {
-    MOTOR_STATE_STOP = 0,
-    MOTOR_STATE_FORWARD,
-    MOTOR_STATE_BACKWARD,
-    MOTOR_STATE_BRAKE
-} MotorState_t;
-
-
-typedef enum {
-    MOTOR_SUBSTATE_NONE = 0,
-    MOTOR_SUBSTATE_ACCEL,
-    MOTOR_SUBSTATE_CONSTANT,
-    MOTOR_SUBSTATE_DECEL
-} MotorSubState_t;
-
+#include <stdbool.h>
 
 typedef struct {
 	GPIO_TypeDef* IN1_port;
-	uint16_t IN1_pin;
-	GPIO_TypeDef* IN2_port;
-	uint16_t IN2_pin;
-	TIM_HandleTypeDef* htim_pwm;
-	uint32_t pwm_channel;
+	    uint16_t IN1_pin;
 
-	MotorState_t state;
-	MotorSubState_t substate;
-	uint32_t state_entry_time_ms;
-	uint8_t target_speed;
-	uint8_t current_speed;
-} Motor_HandleTypeDef;
+	    GPIO_TypeDef* IN2_port;
+	    uint16_t IN2_pin;
 
-void DriverMotors_Init(Motor_HandleTypeDef* motor);
-void DriverMotors_Forward(Motor_HandleTypeDef* motor);
-void DriverMotors_Backward(Motor_HandleTypeDef* motor);
-void DriverMotors_Stop(Motor_HandleTypeDef* motor);
-void DriverMotors_SetSpeed(Motor_HandleTypeDef* motor, uint8_t speed_percent);
-void DriverMotors_TimerTask(Motor_HandleTypeDef* motor);
+	    TIM_HandleTypeDef* htim_pwm;
+	    uint32_t pwm_channel;
+
+	    bool enabled;
+} drvMotor_t;
+
+void DriverMotor_Init(drvMotor_t* motor);
+void DriverMotor_Enable(drvMotor_t* motor);
+void DriverMotor_Disable(drvMotor_t* motor);
+void DriverMotor_setDirection(drvMotor_t* motor, bool forward);
+void DriverMotor_setPwm(drvMotor_t* motor, uint16_t pwm);
 
 #endif
