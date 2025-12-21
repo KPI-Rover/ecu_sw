000d848c2eef8c9f481fbd9247cca59c1fb220f9
rhinemann <andreyfrog26@gmail.com>
2025-12-21 18:29:59 +0100

    Control flow reshufle

--- KPI_Rover/Communication/ProtocolHandler.c
+++ KPI_Rover/Communication/ProtocolHandler.c
@@ -1,2 +1,35 @@
+#include "ProtocolHandler.h"
+#include "cmsis_os.h"
+#include "UARTTransport.h"
 
+//osMessageQueueId_t messageQueue;
+
+void ProtocolHandler_run() {
+	uint8_t msg_ptr;
+	osMessageQueueGet(messageQueue, &msg_ptr, NULL, 0);
+	ProtocolHandler_processRequest(&msg_ptr);
+}
+
+void ProtocolHandler_processRequest(uint8_t *request){
+	enum COMMAND_ID command_id = request[0];
+	uint8_t *payload = request + 1;
+	(void) payload;
+
+	switch (command_id) {
+		case GET_API_VERSION:
+			break;
+		case SET_MOTOR_SPEED:
+			break;
+		case SET_ALL_MOTORS_SPEED:
+			break;
+		case GET_ENCODER:
+			break;
+		case GET_ALL_ENCODERS:
+			break;
+		case CONNECT_UDP:
+			break;
+		default:
+			break;
+	}
+}
 
--- KPI_Rover/Communication/ProtocolHandler.h
+++ KPI_Rover/Communication/ProtocolHandler.h
@@ -1,6 +1,9 @@
 #ifndef COMMUNICATION_PROTOCOLHANDLER_H_
 #define COMMUNICATION_PROTOCOLHANDLER_H_
 
+#include "stdint.h"
 
+void ProtocolHandler_run();
+void ProtocolHandler_processRequest(uint8_t *request);
 
 #endif /* COMMUNICATION_PROTOCOLHANDLER_H_ */
