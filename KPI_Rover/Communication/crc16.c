f06478429424c8c60e5f41c7b47a65ecf9a890ff
rhinemann <andreyfrog26@gmail.com>
2026-01-06 18:09:03 +0100

    Issue #18: CRC16 lookup table added

--- KPI_Rover/Communication/crc16.c
+++ KPI_Rover/Communication/crc16.c
@@ -1,11 +1,32 @@
 #include "crc16.h"
 
+uint16_t crc_table[UINT8_MAX];
+
 uint16_t crc16(uint8_t const *data, size_t size) {
   uint16_t crc = 0;
   while (size--) {
-    crc ^= *data++;
-    for (unsigned k = 0; k < 8; k++)
-      crc = crc & 1 ? (crc >> 1) ^ 0x8005 : crc >> 1;
+	  /* XOR-in next input byte into MSB of crc, that's our new intermediate dividend */
+	  uint8_t pos = (uint8_t)( (crc >> 8) ^ *data++); /* equal: ((crc ^ (b << 8)) >> 8) */
+	  /* Shift out the MSB used for division per lookuptable and XOR with the remainder */
+	  crc = (uint16_t)((crc << 8) ^ (uint16_t)(crc_table[pos]));
   }
   return crc;
+}
+
+void crc16_fillTable() {
+	const uint16_t generator = 0x8005;
+	for (int dividend = 0; dividend < UINT8_MAX; ++dividend) {
+		uint16_t current_byte = dividend << 8;
+
+		for (uint8_t bit = 0; bit < 8; ++bit) {
+			if ((current_byte & 0x8000) != 0) {
+				current_byte <<= 1;
+				current_byte ^= generator;
+			}
+			else {
+				current_byte <<= 1;
+			}
+		}
+		crc_table[dividend] = current_byte;
+	}
 }
