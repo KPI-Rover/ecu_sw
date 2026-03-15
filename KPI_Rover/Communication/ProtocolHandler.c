f63f3974e8fd54c15f002a97fa770ddd335efeae
hasslesstech <hasslesstech@tutanota.com>
2026-03-15 17:32:18 +0200

    Issue #18: Reduce memory usage of the dispatch table

--- KPI_Rover/Communication/ProtocolHandler.c
+++ KPI_Rover/Communication/ProtocolHandler.c
@@ -7,21 +7,17 @@
 #define SEND_BUFFER_SIZE 64
 #define RECV_BUFFER_SIZE 32
 
+#define LEN(x) ( sizeof(x) / sizeof((x)[0]) )
+
 static uint8_t sendBuffer[SEND_BUFFER_SIZE];
 static uint8_t recvBuffer[RECV_BUFFER_SIZE];
 static uint8_t recvSize;
-
-static void (*dispatch_table[256])(void);
 
 static osThreadAttr_t ta = {
 	.stack_size = 64 * 4
 };
 
-static void dispatch_unsupported(void)
-{
-	sendBuffer[0] = 0xFF;
-	UARTTransport_send(sendBuffer, 1);
-}
+static void dispatch_00(void) {}
 
 static void dispatch_01(void)
 {
@@ -62,6 +58,8 @@ static void dispatch_03(void)
 	UARTTransport_send(sendBuffer, 2);
 }
 
+static void dispatch_04(void) {}
+
 static void dispatch_05(void)
 {
 	sendBuffer[0] = 0x05;
@@ -76,6 +74,16 @@ static void dispatch_06(void)
 	UARTTransport_send(sendBuffer, 53);
 }
 
+static const void (*dispatch_table[])(void) = {
+	dispatch_00,
+	dispatch_01,
+	dispatch_02,
+	dispatch_03,
+	dispatch_04,
+	dispatch_05,
+	dispatch_06,
+};
+
 void ProtocolHandler_processTask(void *d)
 {
 	(void) d;
@@ -84,21 +92,15 @@ void ProtocolHandler_processTask(void *d)
 
 	for ( ; ; ) {
 		UARTTransport_receive(recvBuffer, &recvSize);
+
+		if (recvBuffer[0] >= LEN(dispatch_table))
+			continue;
+
 		dispatch_table[recvBuffer[0]]();
 	}
 }
 
 void ProtocolHandler_init(void)
 {
-	for (uint32_t i = 0; i < 256; i++) {
-		dispatch_table[i] = dispatch_unsupported;
-	}
-
-	dispatch_table[1] = dispatch_01;
-	dispatch_table[2] = dispatch_02;
-	dispatch_table[3] = dispatch_03;
-	dispatch_table[5] = dispatch_05;
-	dispatch_table[6] = dispatch_06;
-
 	(void) osThreadNew(ProtocolHandler_processTask, NULL, &ta);
 }
